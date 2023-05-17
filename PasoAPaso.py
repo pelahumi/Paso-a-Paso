ab8ae1914ba97d6c118a38d13f4f1e50c1596028
Pelayo <phuermij@myuax.com>
2023-05-17 20:26:59 +0200

    cambios

--- PasoAPaso.py
+++ PasoAPaso.py
@@ -1,35 +1,19 @@
 import asyncio
+import aiohttp
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
-import aiohttp
 import sys
 from contextlib import closing
 import http.client
-import timeit
 
-def wget(uri):
-    parsed = urlparse(uri)
-    with closing(http.cilent.HTTPConnection(parsed.netloc)) as conn:
-        path = parsed.path
-        if parsed.query:
-            path += "?" + parsed.query
-        conn.request("GET", path)
-        response = conn.getresponse()
-
-        if response.status != 200:
-            print(response.reason, file=sys.stderr)
-            return
-        print("Respuesta OK")
-        return response.read()
 
 def get_images_src_from_html(html_doc):
-    soup = BeautifulSoup(html_doc, "html,parser")
-    return [img.gert("src") for img in soup.find_all("img")]
+    soup = BeautifulSoup(html_doc, "html.parser")
+    return (img.get("src") for img in soup.find_all("img"))
 
 def get_uri_from_images_src(base_uri, images_src):    
    
-    parsed_base = urlparse(base_uri)    
-    result = []    
+    parsed_base = urlparse(base_uri)       
     for src in images_src:    
         parsed = urlparse(src)    
         if parsed.netloc == '':    
@@ -42,36 +26,100 @@ def get_uri_from_images_src(base_uri, images_src):
                 else:    
                     path = '/' + '/'.join(parsed_base.path.split('/') 
 [:-1]) + '/' + path
-                    result.append(parsed_base.scheme + '://' +   
-parsed_base.netloc + path)    
-        else:    
-            result.append(parsed.geturl())    
-    return result 
+                    yield parsed_base.scheme + "://"+ parsed_base.netloc+path
+            else:
+                yield parsed.geturl()
 
-def download(uri):
-    content = wget(uri)
+
+#Descargar en modo asíncrono
+
+async def main(uri):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(uri) as response:
+            if response.status != 200:
+                return None
+            if response.content_type.startswith("text/"):
+                return await response.text()
+            else:
+                return await response.read()
+
+
+async def wget(session, uri):
+    async with session.get(uri) as response:
+        if response.status != 200:
+            return None
+        if response.content_type.startswith("text/"):
+            return await response.text()
+        else:
+            return await response.read()
+        
+async def download(session, uri):
+    content = await wget(session, uri)
     if content is None:
         return None
-    with open(uri.split("sep")[-1], "wb") as f:
+    with open(uri.split(sep)[-1], "wb") as f:
         f.write(content)
         return uri
 
-def get_images(page_uri):
-    html = wget(page_uri)
+
+#Parsear en modo asíncrono
+
+async def get_images_src_from_html(html_doc):
+    soup = BeautifulSoup(html_doc, "html.parser")
+    for img in soup.find_all("img"):
+        yield img.get("src")
+        await asyncio.sleep(0.001)
+
+async def get_uri_from_images_src(base_uri, images_src):
+    parsed_base = urlparse(base_uri)
+    async for src in images_src:
+        parsed = urlparse(src)
+        if parsed.netloc == "":
+            path = parsed.path
+            if parsed.query:
+                path += "?" + parsed.query
+            if path[0] != "/":
+                if parsed.path == "/":
+                    path = "/" + path
+                else:
+                    path = "/" + "/".join(parsed.path.split("/")[:-1]) + "/" + path
+                    yield parsed_base.scheme + "://" + parsed_base.netloc + path
+        else:
+            yield parsed.geturl()
+        await asyncio.sleep(0.001)
+
+
+#Hacer varios tratamientos en modo asíncrono
+
+async def get_images(session, page_uri):
+    html = await wget(session, page_uri)
     if not html:
-        print("Error: no se ha encontrado ninguna imagen", sys.stderr)
+        print("Error: no se ha encontrado ninguna imagen.", sys.stderr)
         return None
     images_src_gen = get_images_src_from_html(html)
     images_uri_gen = get_uri_from_images_src(page_uri, images_src_gen)
-    for image_uri in images_uri_gen:
-        print("Descaga de %s" % image_uri)
-        download(image_uri)
+    async for image_uri in images_src_gen:
+        print("Descarga de %s" % image_uri)
+        await download(session, image_uri)
+
+async def main():
+    web_page_uri = 'http://www.formation-python.com/'
+    async with aiohttp.ClientSession() as session:
+        await get_images(session, web_page_uri)
+
+asyncio.run(main())
 
 
+#Utilizar un ejecutor
 
-if __name__ == '__main__':    
-    print('--- Starting standard download ---')    
-    web_page_uri = 'http://www.formation-python.com/'    
-    print(timeit.timeit('get_images(web_page_uri)',    
-                 number=10,    
-                 setup="from __main__ import get_images, web_page_uri")) 
+def write_in_file(filename, content):
+    with open(filename, "wb") as f:
+        f.write(content)
+
+async def download(session, uri):
+    content = await wget(session, uri)
+    if content is None:
+        return None
+    loop = asyncio.get_running_loop()
+    await loop.run_in_executor(None, partial(write_in_file, uri.split(sep)[-1], content))
+    return uri
